569261a8fc3eed47d84fd662169b504f368bbe55
ALYSSA NICOLE JANSSEN <52120159+AlyssaJanssen@users.noreply.github.com>
2023-03-03 21:40:45 -0700

    Added route to sign up

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,12 +7,11 @@
       <div class="bg-gray-700">
         <p class="flex justify-center items-center">
           <span class="">Register for Free</span>
-          <button
-            type="button"
-            class="mx-2 text-sm bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-2 rounded"
-          >
-            Sign Up
-          </button>
+          <RouterLink
+        to="/register"
+        class="link shadow-xl text-sm mx-2 bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-2 rounded"
+        >Sign Up</RouterLink
+      >
         </p>
       </div>
     </div>
