eaecee9bc329fa9d735f25e275ddf32d75a98c52
ALYSSA NICOLE JANSSEN <52120159+AlyssaJanssen@users.noreply.github.com>
2023-05-02 10:32:03 -0600

    changing book search result #

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -72,7 +72,7 @@ export default {
       books: [],
       keyword: "",
       orderBy: "relevance",
-      maxResults: "24",
+      maxResults: "28",
       loadState: "",
       countryName: "US",
     };
